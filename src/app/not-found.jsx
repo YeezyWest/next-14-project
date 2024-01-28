65ff48ffed3773293650a14072293cc9a1b30621
Yusuf olatunji adeshina <yadeshina57@gmai.com>
2024-01-28 16:01:26 +0100

    feat: app router and layout

---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div>
+        <h2>Not Found</h2>
+        <p>sorry, the page you are looking for does not exists.</p>
+    </div>
+  )
+}
+
+export default NotFound
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Links from './links/Links'
+
+function Navbar() {
+  return (
+    <div><Links /></div>
+  )
+}
+
+export default Navbar
